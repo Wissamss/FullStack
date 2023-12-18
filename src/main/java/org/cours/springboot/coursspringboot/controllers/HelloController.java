3c180d78ce81b59d138c8ad43cad057cb85b90d9
Wissam <wissamsaidicontact@gmail.com>
2023-12-19 00:53:06 +0100

    change method's name

--- src/main/java/org/cours/springboot/coursspringboot/controllers/HelloController.java
+++ src/main/java/org/cours/springboot/coursspringboot/controllers/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @RequestMapping("/")
-    public String bienvenueSpring(){
+    public String bienvenidogSpring(){
         return "Bienvenido";
     }
 }
