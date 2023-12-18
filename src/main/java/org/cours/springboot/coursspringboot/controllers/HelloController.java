cba4799c8d52d08c2fe358a4235d99e027e7c713
imane <imanesti@outlook.com>
2023-12-18 20:54:37 +0100

    imane 3awtani

--- src/main/java/org/cours/springboot/coursspringboot/controllers/HelloController.java
+++ src/main/java/org/cours/springboot/coursspringboot/controllers/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/")
     public String bienvenueSpring(){
-        return "Bienvenido 3ndna";
+        return "Bienvenido 3ndna 3awtano";
     }
 }
