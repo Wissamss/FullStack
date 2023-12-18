6ddef51766743bf9c327512659580699d1ffaa22
Wissam <wissamsaidicontact@gmail.com>
2023-12-18 20:22:44 +0100

    Second commit

--- src/main/java/org/cours/springboot/coursspringboot/controllers/VoitureController.java
+++ src/main/java/org/cours/springboot/coursspringboot/controllers/VoitureController.java
@@ -10,7 +10,6 @@ import java.util.Optional;
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class VoitureController {
-    @Autowired
     private VoitureRepository voitureRepository;
 
     @RequestMapping("/voitures")
